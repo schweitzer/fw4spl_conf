15ee682d4387e8ec8ebd560aea5dad047ee7529d
marc.schweitzer <marc.schweitzer@ircad.u-strasbg.fr>
2015-07-23 18:44:57 +0200

    Updated script, added cmake configuration step

--- configuration.sh
+++ configuration.sh
@@ -8,6 +8,11 @@ echo -n "Are you sure you want to build FW4SPL environement in this directory [y
 read continu
 
 latest_version=fw4spl_0.10.2.1
+pathExtDeps=""
+pathAr=""
+pathExtDeps=""
+pathExt=""
+numberofCore=grep -c ^processor /proc/cpuinfo
 
 if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
 
@@ -42,6 +47,9 @@ if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
         git clone https://github.com/fw4spl-org/fw4spl-ext-deps.git ./BinPkgs/Src/fw4spl-ext-deps
         git clone https://github.com/fw4spl-org/fw4spl.git ./Src/fw4spl-ext
 
+        pathExtDeps=./BinPkgs/Src/fw4spl-ext-deps
+        pathExt=./Src/fw4spl-ext
+
         cd ./BinPkgs/Src/fw4spl-ext-deps
         git checkout $latest_version
         cd ../../..
@@ -55,6 +63,9 @@ if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
         git clone https://github.com/fw4spl-org/fw4spl-ar-deps.git ./BinPkgs/Src/fw4spl-ar-deps
         git clone https://github.com/fw4spl-org/fw4spl-ar.git ./Src/fw4spl-ar
 
+        pathArDeps=./BinPkgs/Src/fw4spl-ar-deps
+        pathAr=./Src/fw4spl-ar
+
         cd ./BinPkgs/Src/fw4spl-ar-deps
         git checkout $latest_version
         cd ../../..
@@ -62,6 +73,21 @@ if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
         git checkout $latest_version
         cd ../..
     fi
+
+    echo "cmake configuration of dependencies ..."
+
+    cd ./BinPkgs/Build/Debug
+    cmake ../../Src/fw4spl-deps -DCMAKE_INSTALL_PREFIX=../Install/Debug -DCMAKE_BUILD_TYPE=Debug
+    -DADDITIONAL_PROJECTS=$pathExtDeps;$pathArDeps
+
+    cd ../Release
+    cmake ../../Src/fw4spl-deps -DCMAKE_INSTALL_PREFIX=../Install/Release -DCMAKE_BUILD_TYPE=Release
+    -DADDITIONAL_PROJECTS=$pathExtDeps;$pathArDeps
+
+    
+
+    #to the root
+    cd ../../..
 
 
     echo "done"
