033e9d90ab3bf7b10fba2650122171cc33fabd50
marc.schweitzer <marc.schweitzer@ircad.u-strasbg.fr>
2015-07-24 16:49:44 +0200

    Updated config script: Added color, added test of required program, added first version of building step

--- configuration.sh
+++ configuration.sh
@@ -3,40 +3,71 @@
 
 echo FW4SPL configuration
 
+echo -e "\033[33;34m"
+echo "checking for required programs"
+echo -e "\033[33;37m"
 
-echo -n "Are you sure you want to build FW4SPL environement in this directory [y|n] ?"
-read continu
 
+command -v git >/dev/null 2>&1 || { echo >&2 "git required but installed.  Aborting."; exit 1; }
+command -v cmake >/dev/null 2>&1 || { echo >&2 "cmake required but installed.  Aborting."; exit 1; }
+command -v make >/dev/null 2>&1 || { echo >&2 "make required but installed.  Aborting."; exit 1; }
+
+echo -e "\033[33;34m"
+echo "Checking version..."
+echo -e "\033[33;37m"
+
+rootPath=$(pwd)
 latest_version=fw4spl_0.10.2.1
 pathExtDeps=""
 pathAr=""
 pathExtDeps=""
 pathExt=""
-numberofCore=grep -c ^processor /proc/cpuinfo
+numberOfCore=$(grep -c ^processor /proc/cpuinfo)
+
+echo -e "\033[33;32m"
+echo "FW4SPL auto-configuration tools"
+echo "- git found : $(which git)"
+echo "- cmake found : $(which cmake)"
+echo "- make found : $(which make)"
+echo "- root directory : $rootPath"
+echo "- lastest version is : $latest_version"
+echo "- number of Cores : $numberOfCore"
+
+echo -e "\033[33;37m"
+
+
+echo -n "\ Are you sure you want to build FW4SPL environement in this directory ($pwd) [y|n] ?"
+read continu
+
+
 
 if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
 
-    echo "Creation of folders ..."
+    echo -e "\033[33;34m"
+    echo "Creation of folders..."
+    echo -e "\033[33;37m"
 
     mkdir Src Build Install
     mkdir Build/Debug Build/Release
     mkdir Install/Debug Install/Release
 
-    mkdir BinPkgs
-    mkdir BinPkgs/Src BinPkgs/Build BinPkgs/Install BinPkgs/Build/Debug BinPkgs/Build/Release BinPkgs/Install/Debug BinPkgs/Install/Release
+    mkdir Deps
+    mkdir Deps/Src Deps/Build Deps/Install Deps/Build/Debug Deps/Build/Release Deps/Install/Debug Deps/Install/Release
 
     echo -n "Do you want 'Extended' repositories [y|n] ? "
     read extended
 
-    echo -n "Do you want 'Augmented Reality' repositories [y|n] ?"
+    echo -n  "Do you want 'Augmented Reality' repositories [y|n] ?"
     read ar
 
+    echo -e "\033[33;34m"
     echo "cloning repositories..."
+    echo -e "\033[33;37m"
 
-    git clone https://github.com/fw4spl-org/fw4spl-deps.git ./BinPkgs/Src/fw4spl-deps
+    git clone https://github.com/fw4spl-org/fw4spl-deps.git ./Deps/Src/fw4spl-deps
     git clone https://github.com/fw4spl-org/fw4spl.git ./Src/fw4spl
 
-    cd ./BinPkgs/Src/fw4spl-deps
+    cd ./Deps/Src/fw4spl-deps
     git checkout $latest_version
     cd ../../..
     cd ./Src/fw4spl
@@ -44,13 +75,13 @@ if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
     cd ../..
 
     if [ "$extended" = "y" ] || [ "$extended" = "Y" ]; then
-        git clone https://github.com/fw4spl-org/fw4spl-ext-deps.git ./BinPkgs/Src/fw4spl-ext-deps
+        git clone https://github.com/fw4spl-org/fw4spl-ext-deps.git ./Deps/Src/fw4spl-ext-deps
         git clone https://github.com/fw4spl-org/fw4spl.git ./Src/fw4spl-ext
 
-        pathExtDeps=./BinPkgs/Src/fw4spl-ext-deps
-        pathExt=./Src/fw4spl-ext
+        pathExtDeps="$rootPath/Deps/Src/fw4spl-ext-deps"
+        pathExt="$rootPath/Src/fw4spl-ext"
 
-        cd ./BinPkgs/Src/fw4spl-ext-deps
+        cd ./Deps/Src/fw4spl-ext-deps
         git checkout $latest_version
         cd ../../..
         cd ./Src/fw4spl-ext
@@ -60,13 +91,13 @@ if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
     fi
 
     if [ "$ar" = "y" ] || [ "$ar" = "Y" ]; then
-        git clone https://github.com/fw4spl-org/fw4spl-ar-deps.git ./BinPkgs/Src/fw4spl-ar-deps
+        git clone https://github.com/fw4spl-org/fw4spl-ar-deps.git ./Deps/Src/fw4spl-ar-deps
         git clone https://github.com/fw4spl-org/fw4spl-ar.git ./Src/fw4spl-ar
 
-        pathArDeps=./BinPkgs/Src/fw4spl-ar-deps
-        pathAr=./Src/fw4spl-ar
+        pathArDeps="$rootPath/Deps/Src/fw4spl-ar-deps"
+        pathAr="$rootPath/Src/fw4spl-ar"
 
-        cd ./BinPkgs/Src/fw4spl-ar-deps
+        cd ./Deps/Src/fw4spl-ar-deps
         git checkout $latest_version
         cd ../../..
         cd ./Src/fw4spl-ar
@@ -74,21 +105,30 @@ if [ "$continu" = "y" ] || [ "$continu" = "Y" ]; then
         cd ../..
     fi
 
+    echo -e "\033[33;34m"
     echo "cmake configuration of dependencies ..."
+    echo -e "\033[33;37m"
 
-    cd ./BinPkgs/Build/Debug
-    cmake ../../Src/fw4spl-deps -DCMAKE_INSTALL_PREFIX=../Install/Debug -DCMAKE_BUILD_TYPE=Debug
-    -DADDITIONAL_PROJECTS=$pathExtDeps;$pathArDeps
+    echo "fw4spl-ext-deps : $pathExtDeps"
+    echo "fw4spl-ar-deps : $pathArDeps"
+
+    cd ./Deps/Build/Debug
+    cmake $rootPath/Deps/Src/fw4spl-deps -DCMAKE_INSTALL_PREFIX=$rootPath/Deps/Install/Debug -DCMAKE_BUILD_TYPE=Debug -DADDITIONAL_DEPS="$pathExtDeps;$pathArDeps"
 
     cd ../Release
-    cmake ../../Src/fw4spl-deps -DCMAKE_INSTALL_PREFIX=../Install/Release -DCMAKE_BUILD_TYPE=Release
-    -DADDITIONAL_PROJECTS=$pathExtDeps;$pathArDeps
-
-    
+    cmake $rootPath/Deps/Src/fw4spl-deps -DCMAKE_INSTALL_PREFIX=$rootPath/Deps/Install/Release -DCMAKE_BUILD_TYPE=Release -DADDITIONAL_DEPS="$pathExtDeps;$pathArDeps"
 
     #to the root
     cd ../../..
 
+    echo "Configuration done."
+    echo -n "Do you want to build [y|n] ?"
+    read build
+
+    if [ "$build" = "y" ] || [ "$build" = "Y" ]; then
+        cd ./Deps/Build/Debug
+        make all -j$numberOfCore
+    fi
 
     echo "done"
 
